02b55f47d9e27ca00198ce532e6cda4e612b079e
nskij <n.skij0123@gmail.com>
2022-03-07 11:33:23 +0900

    feat: 1-3回答

---
+++ src/Level1/practice1_3.tsx
@@ -0,0 +1,12 @@
+type IsPositiveFunc = {
+  (num: number): boolean;
+};
+
+const isPositive: IsPositiveFunc = (num) => num >= 0;
+
+// 使用例
+isPositive(5);
+
+// エラー例
+// isPositive("foo");
+// const res: number = isPositive(123);
