4fe40e08342a2580c4e6210fa0bf8908e40e9f08
nskij <n.skij0123@gmail.com>
2022-03-15 12:48:06 +0900

    feat: 2-4回答

---
+++ src/Level2/practice2_4.tsx
@@ -0,0 +1,34 @@
+// プロパティを1つ増やす関数
+// 下のコードで定義されるgiveId関数は、オブジェクトを受け取って、それに新しい文字列型のプロパティidを足してできる新しいオブジェクトを返す関数です。
+// この関数に適切な型を付けてください。
+// なお、簡単のために、giveIdに渡されるオブジェクトobjが既にidプロパティを持っている場合は考えなくて構いません。
+
+function giveId<T>(obj: T): T & { id: string } {
+  const id = "本当はランダムがいいけどここではただの文字列";
+  return {
+    ...obj,
+    id,
+  };
+}
+
+// 使用例
+const obj1: {
+  id: string;
+  foo: number;
+} = giveId({ foo: 123 });
+const obj2: {
+  id: string;
+  num: number;
+  hoge: boolean;
+} = giveId({
+  num: 0,
+  hoge: true,
+});
+
+// エラー例
+// const obj3: {
+//   id: string;
+//   piyo: string;
+// } = giveId({
+//   foo: "bar",
+// });
